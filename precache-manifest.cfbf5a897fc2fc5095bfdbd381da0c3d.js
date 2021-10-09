b9ea58c2d61a782b1096ea43b1ec15d05dd524d8
LuisP <luisjavierpolancomorel@gmail.com>
2021-10-09 00:43:13 -0400

    Updates

---
+++ precache-manifest.cfbf5a897fc2fc5095bfdbd381da0c3d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5029ab29041a9e56d1f0e3ab2ec1ebed",
+    "url": "/ip-address-tracker/index.html"
+  },
+  {
+    "revision": "bf4767e5a58dc3506a6b",
+    "url": "/ip-address-tracker/static/css/main.19e6585e.chunk.css"
+  },
+  {
+    "revision": "66be90cc6764f525276f",
+    "url": "/ip-address-tracker/static/js/2.819fcd8f.chunk.js"
+  },
+  {
+    "revision": "3c189f022372ef498421b6b1e392503b",
+    "url": "/ip-address-tracker/static/js/2.819fcd8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4767e5a58dc3506a6b",
+    "url": "/ip-address-tracker/static/js/main.f9e193a8.chunk.js"
+  },
+  {
+    "revision": "5edf9300e4981cca50c1",
+    "url": "/ip-address-tracker/static/js/runtime-main.b78ae688.js"
+  },
+  {
+    "revision": "53ef8ed8ef4b92721f00cb8bfdc5c9b9",
+    "url": "/ip-address-tracker/static/media/icon-arrow.53ef8ed8.svg"
+  },
+  {
+    "revision": "ff17146919959b508ee5dd9af2f9f9e5",
+    "url": "/ip-address-tracker/static/media/icon-location.ff171469.svg"
+  },
+  {
+    "revision": "8d3f4ad64bf3a9320e172edf01e0c016",
+    "url": "/ip-address-tracker/static/media/pattern-bg.8d3f4ad6.png"
+  }
+]);
